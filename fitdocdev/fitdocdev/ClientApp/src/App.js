3ac3998fcac5287a80d5abf5d4eb1e73bb66455c
silentcat21 <86968099+silentcat21@users.noreply.github.com>
2022-01-21 20:55:00 +0900

    2022.01.21

--- fitdocdev/fitdocdev/ClientApp/src/App.js
+++ fitdocdev/fitdocdev/ClientApp/src/App.js
@@ -7,7 +7,7 @@ import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
-
+import { Component } from 'react';
 import './custom.css'
 
 export default class App extends Component {
