e54ef888325aca625799c8980a81fe5fdd1aa6f4
silentcat21 <86968099+silentcat21@users.noreply.github.com>
2022-03-16 21:14:04 +0900

    2022.03.16

---
+++ front/components/AppLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppLayout = () => {
+    return (
+        <div></div>
+    );
+}
+
+AppLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default AppLayout;
